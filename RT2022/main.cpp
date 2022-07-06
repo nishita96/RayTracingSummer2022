920aef172ac73ad4f831cb9e27576d59935f4387
Nishita <nishitakharche@gmail.com>
2022-07-06 03:08:56 -0400

    Adding adjustable field of view. Camera parameters.

--- RT2022/main.cpp
+++ RT2022/main.cpp
@@ -59,26 +59,21 @@ int main() {
     const int samples_per_pixel = 100;
     const int max_depth = 50;
     
-    //objects in world 
+    //Objects in world
     hittable_list world;
-//    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-//    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-    
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-//    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-//    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
-                                                   
+
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0, 0.0, -1.0), 0.4, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.6, material_left));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.5, material_left));//negative refractive index
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.5, material_left));//negative refractive index for glass/bubble
     world.add(make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.4, material_right));
     
     //camera things
-    camera cam;
+    camera cam(90.0, aspectRatio);
     
     //rendering
     
